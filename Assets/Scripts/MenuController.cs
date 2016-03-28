75197006c419be50bf8caf0d199d20900a23acca
yusufmatahari <yusuf.matahari06@gmail.com>
2016-03-28 15:44:56 +0700

    Ucup Update

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,50 +4,76 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
 
-	//ini panel
-	public GameObject MenuPanel;
-	public GameObject ProfilePanel;
-	public GameObject SettingPanel;
+    //inisialisasi panel
+    public GameObject MenuPanel;
+    public GameObject PlayPanel;
+    public GameObject ProfilePanel;
+    public GameObject ScorePanel;
+    public GameObject SettingPanel;
 
-	//ini button
-	public Button ButtonProfile;
-	public Button ButtonSetting;
-	public Button[] ButtonBack; 
-
+    //inisialisasi tombol
+    public Button ButtonPlay;
+    public Button ButtonProfile;
+    public Button ButtonScore;
+    public Button ButtonSetting;
+    public Button[] ButtonBack;
 
 	// Use this for initialization
 	void Start () {
-		MenuPanel.SetActive (true);
-		ProfilePanel.SetActive (false);
+        MenuPanel.SetActive(true);
+        PlayPanel.SetActive(false);
+        ProfilePanel.SetActive(false);
+        ScorePanel.SetActive(false);
+        SettingPanel.SetActive(false);
 
-		ButtonProfile.onClick.AddListener ( delegate { pergiKePanel(1); } );
-		ButtonSetting.onClick.AddListener (delegate { pergiKePanel(2); } );
-		ButtonBack[0].onClick.AddListener ( delegate { pergiKePanel (0); } );
-		ButtonBack[1].onClick.AddListener ( delegate { pergiKePanel (0); } );
+        ButtonPlay.onClick.AddListener(delegate { pergiKePanel(1); });
+        ButtonProfile.onClick.AddListener(delegate { pergiKePanel(2); });
+        ButtonScore.onClick.AddListener(delegate { pergiKePanel(3); });
+        ButtonSetting.onClick.AddListener(delegate { pergiKePanel(4); });
+        //ButtonBack.onClick.AddListener(delegate { pergiKePanel(0); });
+        for (int i = 0; i < ButtonBack.Length; i++) {
+            ButtonBack[i].onClick.AddListener(delegate { pergiKePanel(0); });
+        }
 	}
 
-	public void pergiKePanel(int panelId){
-		//id 0 = menu
-		//id 1 = profile
-		//id 2 = setting
-		if (panelId == 0) { //go to menu
-			MenuPanel.SetActive (true); 
-			ProfilePanel.SetActive (false);
-			SettingPanel.SetActive (false);
-		}
-
-		if (panelId == 1) { //go to profile
-			MenuPanel.SetActive (false);
-			ProfilePanel.SetActive (true);
-			SettingPanel.SetActive (false);
-		}
-
-		if (panelId == 2) {
-			MenuPanel.SetActive (false);
-			ProfilePanel.SetActive (false);
-			SettingPanel.SetActive (true);
-		}
-	}
-	
-
+    public void pergiKePanel(int panelId)
+    {
+        if (panelId == 0) {
+            MenuPanel.SetActive(true);
+            PlayPanel.SetActive(false);
+            ProfilePanel.SetActive(false);
+            ScorePanel.SetActive(false);
+            SettingPanel.SetActive(false);
+        }
+        if (panelId == 1)
+        {
+            PlayPanel.SetActive(true);
+            MenuPanel.SetActive(false);
+            ProfilePanel.SetActive(false);
+            ScorePanel.SetActive(false);
+            SettingPanel.SetActive(false);
+        }
+        if (panelId == 2)
+        {
+            ProfilePanel.SetActive(true);
+            MenuPanel.SetActive(false);
+            PlayPanel.SetActive(false);
+            ScorePanel.SetActive(false);
+            SettingPanel.SetActive(false);
+        }
+        if (panelId == 3) {
+            ScorePanel.SetActive(true);
+            MenuPanel.SetActive(false);
+            PlayPanel.SetActive(false);
+            ProfilePanel.SetActive(false);
+            SettingPanel.SetActive(false);
+        }
+        if (panelId == 4) {
+            SettingPanel.SetActive(true);
+            MenuPanel.SetActive(false);
+            PlayPanel.SetActive(false);
+            ProfilePanel.SetActive(false);
+            ScorePanel.SetActive(false);
+        }
+    }
 }
