b336c29f16019fa2077fb2004bd0d6fdd17404fe
yusufmatahari <yusuf.matahari06@gmail.com>
2016-04-01 21:15:37 +0700

    diceController

---
+++ Assets/Scripts/DiceController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceController : MonoBehaviour {
+
+    public int PutarDadu()
+    {
+        int dadu = Random.Range(1, 7);
+        return dadu;
+    }
+}
