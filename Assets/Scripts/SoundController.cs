275f06b90d8afa1811194b60d3c933b43e0cba83
yusufmatahari <yusuf.matahari06@gmail.com>
2016-04-09 22:26:38 +0700

    setting page, buttons, bgm, toggle button music on / off : working

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+{
+
+    public Button ButtonMusic;
+    public GameObject audioSource;
+    public bool suara = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        ButtonMusic.onClick.AddListener(delegate { pergiKeFungsi(1); });
+    }
+
+    // Update is called once per frame
+    public void pergiKeFungsi(int fungsiId)
+    {
+        if (fungsiId == 1)
+        {
+            if (suara == true)
+            {
+                AudioListener.volume = 0.0f;
+                ButtonMusic.GetComponentInChildren<Text>().text = "Music : Off";
+                suara = false;
+            }
+            else {
+                AudioListener.volume = 1.0f;
+                ButtonMusic.GetComponentInChildren<Text>().text = "Music : On";
+                suara = true;
+            }
+        }
+    }
+}
