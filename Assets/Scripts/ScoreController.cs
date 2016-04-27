ab20262eddb9bf4ddd2bd88fa49d1954e881eebe
taufiqtab <taufiqtab77@gmail.com>
2016-04-27 20:58:23 +0700

    Score Controller by Abdie

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	int intScore;
+	int intBestScore;
+	GooglePlayServices_Access gps;
+
+	// Use this for initialization
+	void Start () {
+		intBestScore = PlayerPrefs.GetInt ("score");
+		gps = this.GetComponent<GooglePlayServices_Access> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void addScore(int score){
+		intScore += score;
+
+		if (intScore > intBestScore) {
+			intBestScore = intScore;
+			PlayerPrefs.SetInt ("score", intBestScore);
+		}
+	}
+
+	void winStatus(int id){
+		if(id == 1){
+			//menang
+		}
+		if(id == 2){
+			//kalah
+		}
+		gps.tambahScore (intBestScore);
+	}
+}
